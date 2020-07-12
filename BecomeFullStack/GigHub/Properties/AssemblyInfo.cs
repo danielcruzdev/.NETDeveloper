6b80204353688dfc7681fe70aa1fd3e351e43bc8
Daniel Cruz <d.afc98@gmail.com>
2020-07-12 14:32:15 -0300

    Delete and create the right project with authentication

--- BecomeFullStack/GigHub/Properties/AssemblyInfo.cs
+++ BecomeFullStack/GigHub/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("7ab94f49-0a50-4225-a924-88293ddd5a3f")]
+[assembly: Guid("14eeb96a-1141-4107-8901-53c07eb5c2e6")]
 
 // Version information for an assembly consists of the following four values:
 //
